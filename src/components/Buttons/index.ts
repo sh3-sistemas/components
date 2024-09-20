c87222b7136be85a7445568bd409d1db5cf4bda1
Mariana Castro <mariana.castro.sh3@gmail.com>
2024-09-20 15:04:18 -0300

    feature(button): criação do sh3-panel-button

--- src/components/Buttons/index.ts
+++ src/components/Buttons/index.ts
@@ -1,6 +1,7 @@
 import Sh3Button from "./Sh3Button.vue";
 import Sh3IconButton from "./Sh3IconButton.vue";
-import Sh3SubmitButton from './Deprecated/Sh3SubmitButton.vue';
+import Sh3PanelButton from "./Sh3PanelButton.vue";
+import Sh3SubmitButton from "./Deprecated/Sh3SubmitButton.vue";
 
-export { Sh3Button, Sh3IconButton, Sh3SubmitButton };
-export * from './types';
+export { Sh3Button, Sh3IconButton, Sh3PanelButton, Sh3SubmitButton };
+export * from "./types";
--- src/components/Buttons/types/index.ts
+++ src/components/Buttons/types/index.ts
@@ -1,22 +1,20 @@
 /**
- *
- * Sh3IconButton is an extension to standard button element with icons and theming.
- *
- * [Primevue Based](https://www.primevue.org/button/)
- *
  * @module buttons
- *
  */
 import type { ButtonEmits, ButtonProps, ButtonSlots } from "primevue/button";
-import type { ClassComponent, GlobalComponentConstructor } from "primevue/ts-helpers";
+import type { OverlayPanelEmits, OverlayPanelProps, OverlayPanelSlots } from "primevue/overlaypanel";
+import type {
+  ClassComponent,
+  GlobalComponentConstructor,
+} from "primevue/ts-helpers";
 import type { IconProps } from "@/types";
 
 /**
- * Defines valid properties in Button component.
+ * Defines valid properties in IconButton component.
  */
 export type Sh3IconButtonProps = {
-    icon: IconProps;
-}
+  icon: IconProps;
+};
 
 /**
  * **SH3 - Sh3IconButton**
@@ -27,7 +25,11 @@ export type Sh3IconButtonProps = {
  * @group Component
  *
  */
-declare class Sh3IconButton extends ClassComponent<Sh3IconButtonProps & Omit<ButtonProps, 'icon'>, ButtonSlots, ButtonEmits> { }
+declare class Sh3IconButton extends ClassComponent<
+  Sh3IconButtonProps & Omit<ButtonProps, "icon">,
+  ButtonSlots,
+  ButtonEmits
+> {}
 /**
  * **SH3 - Sh3Button**
  *
@@ -37,11 +39,39 @@ declare class Sh3IconButton extends ClassComponent<Sh3IconButtonProps & Omit<But
  * @group Component
  *
  */
-declare class Sh3Button extends ClassComponent<ButtonProps, ButtonSlots, ButtonEmits> { }
+declare class Sh3Button extends ClassComponent<
+  ButtonProps,
+  ButtonSlots,
+  ButtonEmits
+> {}
 
-declare module 'vue' {
-    export interface GlobalComponents {
-        Sh3IconButton: GlobalComponentConstructor<Sh3IconButton>;
-        Sh3Button: GlobalComponentConstructor<Sh3Button>;
-    }
+/**
+ * Defines valid properties in PanelButton component.
+ */
+export type Sh3PanelButtonProps = {
+    buttonProps: Sh3IconButtonProps;
+    panelProps: OverlayPanelProps;
+  };
+
+/**
+ * **SH3 - Sh3PanelButton**
+ *
+ * _PanelButton is an extension to standard OverlayPanel element with icons and theming._
+ * --- ---
+ * [Primevue Based Component](https://www.primevue.org)
+ * @group Component
+ *
+ */
+declare class Sh3PanelButton extends ClassComponent<
+  Sh3PanelButtonProps,
+  ButtonSlots & OverlayPanelSlots,
+  ButtonEmits & OverlayPanelEmits
+> {}
+
+declare module "vue" {
+  export interface GlobalComponents {
+    Sh3IconButton: GlobalComponentConstructor<Sh3IconButton>;
+    Sh3Button: GlobalComponentConstructor<Sh3Button>;
+    Sh3PanelButton: GlobalComponentConstructor<Sh3PanelButton>;
+  }
 }
