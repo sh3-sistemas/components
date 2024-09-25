2d2fb6201abe9dde752cfbc180d76d6a4dd1a890
guilherme-batista-sh3 <guilherme.evangelista@sh3.com.br>
2024-09-25 17:10:26 -0300

    feature(component): Adicionar componente DataTableEditable #6

--- src/components/Tables/index.ts
+++ src/components/Tables/index.ts
@@ -2,5 +2,6 @@ import Sh3DataTable from "./DataTable/Sh3DataTable.vue";
 import Sh3DataTableEditable from "./DataTable/Sh3DataTableEditable.vue";
 import Sh3FilterTags from "./Filters/Sh3FilterTags.vue";
 import usePagination from "./composables/usePagination";
+import Sh3DataTableEditable from "./DataTable/Sh3DataTableEditable.vue";
 
 export { Sh3DataTable, Sh3DataTableEditable, Sh3FilterTags, usePagination };
