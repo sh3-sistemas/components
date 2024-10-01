122e334b08ff400d8683800cce86bd2f1a63a8ec
Mariana Castro <mariana.castro.sh3@gmail.com>
2024-10-01 09:44:57 -0300

    refactor(panelButton): remoção de códigos comentados

--- src/components/Buttons/Sh3PanelButton.vue
+++ src/components/Buttons/Sh3PanelButton.vue
@@ -5,14 +5,6 @@
     </Sh3IconButton>
 
     <OverlayPanel ref="op" v-bind="panelProps">
-      <!-- demonstrate Vue 3 dynamic slot/template pass through -->
-      <!--  <template
-        v-for="(slot, index) of slotNamesOverlay"
-        :key="index"
-        #[slot]="slotProps"
-      >
-        <slot :name="slot" v-bind="{ ...(slotProps as object) }" />
-      </template> -->
       <template #container="slotProps"
         ><slot name="container" v-bind="{ ...slotProps }"></slot
       ></template>
@@ -22,19 +14,12 @@
 <script setup lang="ts">
 import type { Sh3PanelButtonProps } from "@/types";
 import Sh3IconButton from "./Sh3IconButton.vue";
-/* import type { OverlayPanelSlots } from "primevue/overlaypanel"; */
 import OverlayPanel from "primevue/overlaypanel";
 import { ref } from "vue";
 const op = ref();
 const toggle = (event: any) => {
   op.value.toggle(event);
 };
-
-/* const slots = useSlots(); */
-// Assert type here to prevent errors in template
-/* const slotNamesOverlay = Object.keys(
-  slots
-) as (keyof OverlayPanelSlots)[] as unknown; */
 
 defineProps<Sh3PanelButtonProps>();
 </script>
