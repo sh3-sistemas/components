c7865852e80ba1db3ab11df0b632e6ed64b62a98
Bárbara Boechat <barbara.boechat@sh3.com.br>
2024-10-02 09:35:46 -0300

    feature(pagination): Criação de um composable p/ paginação utilizando uma função fetch generica que deve puxar os dados do banco

--- src/components/Tables/composables/usePagination.ts
+++ src/components/Tables/composables/usePagination.ts
@@ -1,28 +1,22 @@
 import type { DataTablePageEvent } from "primevue/datatable"
 import { onMounted, toRefs } from "vue"
-export type ApolloFetchOptions = {
-    clientId: string,
-    limit?: number
-}
+import type { Fetch } from "../types"
 
-export type ApolloFetch = {
-    query: object,
-    filterQuery: object | null,
-    limit: number,
-    options: ApolloFetchOptions
-}
-
-export default function usePagination(fetch: (query: object, filerQuery: object | null, options: object) => Promise<void>, config: ApolloFetch, refetch: (filterQuery: object | null) => Promise<void>) {
+export default function usePagination(fetch: (query: object, filerQuery: object | null, options: object) => Promise<void>, config: Fetch, refetch: (filterQuery: object | null) => Promise<void>) {
     const { query, options, filterQuery } = toRefs(config)
     const limit = options.value.limit ?? 10
+    const page = options.value.page ?? 1
+    const filter = filterQuery?.value ?? {}
+
     onMounted(async () => {
-        await fetch(query, { limit, ...filterQuery.value }, options.value)
+        console.log({ filter, filterQuery })
+        await fetch(query, { limit, page, ...filter }, options.value)
     })
 
     const getPage = async (pageEvent: DataTablePageEvent) => {
         const { page, filters } = pageEvent
         await (refetch as any).value({
-            ...(filterQuery.value ?? {}),
+            ...filter,
             ...filters,
             page,
             limit,
